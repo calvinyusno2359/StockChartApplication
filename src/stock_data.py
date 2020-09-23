d5b69ebece0ff21241154088f24e3cc1110088aa
heavensward.argentlux <heavensward.argentlux@gmail.com>
2020-09-23 23:19:46 +0800

    define StockData object methods and attributes

--- src/stock_data.py
+++ src/stock_data.py
@@ -1,1 +1,16 @@
-print("hello world")
+import pandas as pd
+
+class StockData():
+	def __init__(self, filepath):
+		self.data = pd.read_csv(filepath)
+
+	def get_SMA15(self):
+		return self
+
+	def get_SMA50(self):
+		return self
+
+if __name__ == "__main__":
+	print("hello world")
+	x = StockData("../data/C31.SI.csv")
+	print(x.data)

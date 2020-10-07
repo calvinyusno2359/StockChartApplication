29abfe98c20b9d8c81693c8f3e39545ed7b50870
limwy08 <weiyang.lim.2018@business.smu.edu.sg>
2020-10-07 22:04:04 +0800

    Revert "Merge branch 'master' of https://github.com/heavensward-argentlux/StockChartApplication"
    This reverts commit 3984876d0bbbb7cf936c1816d1c42e43298c423e, reversing
    changes made to bb7d2e678bc4d23b179f3d25f32c7aa9629afb3b.

--- src/stock_data.py
+++ src/stock_data.py
@@ -1,10 +1,9 @@
 import pandas as pd
-import numpy as np
 
 class StockData():
 	def __init__(self, filepath):
 		self.data = self.read_csv(filepath).set_index('Date')
-		self.dataraw = self.read_csv("../data/GOOG.csv")
+		self.dataraw = self.read_csv("./data/GOOG.csv")
 		# self.check_data
 		# self.calculate_SMA
 		# self.calculate_crossover
@@ -20,26 +19,24 @@ class StockData():
 		except IOError as e:
 			raise Exception(e)
 
-	def calculate_SMA(self, n):
+	def calculate_SMA(self, N):
 		"""
 		Given self.data and N, find the SMA(N) and augments self.data with the SMA data as new column
 		"""
-		df = self.dataraw #Extract full dataframe from the actual data(to check if there is enough data for sma)
-		dateList = self.data.index.values.tolist() #List of data in self dataframe
+		df = dataraw #Extract full dataframe from the actual data(to check if there is enough data for sma)
+		dateList = self.index.values.tolist() #List of data in self dataframe
 		returnList = []
 		for date in dateList: #for date in dateList
 			dateIndex = df[df["Date"]==date].index.values[0] # find the index of date in the full data
 	#         print(dateIndex)
 			if dateIndex < n: # if date index is less than n: append None
-				returnList.append(np.nan)
+				returnList.append(None)
 			else:
 				sum = 0
 				for i in range(n):
 					sum += df.iloc[dateIndex-i]["Adj Close"]#else sum of data from dateIndex to dateIndex-i(0,1,2...n)
 				returnList.append(sum/n)  #append the SMA for each day to a list
-		self.data['SMA' + str(n)] = returnList
-		print(self.data)
-		return self #returnlist
+		return returnList #returnlist
 
 	def get_data(self, start_date, end_date):
 		"""
@@ -54,12 +51,8 @@ class StockData():
 
 
 	def check_data(self, filename):
-		# df = self.dataraw
-		nData = self.data.reset_index()
-		newData = nData.interpolate() #function to fill in naan with average with the one previous and after data
-		newData.to_csv(filename, index=False) #overwrite old csv with new clean data csv
-		return newData
-		
+		newData = self.interpolate() #function to fill in naan with average with the one previous and after data
+		df.to_csv(filename, index=False) #overwrite old csv with new clean data csv
 
 	def calculate_crossover(self):
 		pass
@@ -73,17 +66,7 @@ class StockData():
 		return (first, last)
 
 if __name__ == "__main__":
-	# raw = StockData("../data/GOOG2.csv")
-	# selected = raw.get_data('2018-01-02', '2020-09-22')
-	# newDataf = raw.check_data("../data/GOOG.csv")
-	new = StockData("../data/GOOG.csv")
-	print(new.check_data("../data/GOOG.csv"))
-	new2 = StockData("../data/GOOG.csv")
-	# print(new2)
-	new2.calculate_SMA(15)
-	new2.calculate_SMA(50)
-	new2.calculate_SMA(50)
-	# print(raw.data)
-	# print(selected)
-	# print(raw.check_data("../data/GOOG.csv"))
-
+	raw = StockData("../data/GOOG2.csv")
+	selected = raw.get_data('2018-01-02', '2020-09-22')
+	print(raw.data)
+	print(selected)

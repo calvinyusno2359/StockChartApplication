8487418e83f11fd883127d98e5415646c560238f
heavensward.argentlux <heavensward.argentlux@gmail.com>
2020-11-04 19:50:27 +0800

    readded update_canvas

--- src/app.py
+++ src/app.py
@@ -30,9 +30,9 @@ class Main(qtw.QWidget, Ui_Form):
 
 		# button & checkbox connections
 		self.loadCSVButton.clicked.connect(self.load_data)
-		self.updateWindowButton.clicked.connect(self.update_graphics)
-		self.SMA1Checkbox.stateChanged.connect(self.update_graphics)
-		self.SMA2Checkbox.stateChanged.connect(self.update_graphics)
+		self.updateWindowButton.clicked.connect(self.update_canvas)
+		self.SMA1Checkbox.stateChanged.connect(self.update_canvas)
+		self.SMA2Checkbox.stateChanged.connect(self.update_canvas)
 
 		self.scrollwidget = qtw.QWidget()
 		self.scrollLayout = qtw.QVBoxLayout()
@@ -75,6 +75,94 @@ class Main(qtw.QWidget, Ui_Form):
 		except TypeError as e:
 			self.report(f"The return tuple is probably (nan, nan) because .csv is empty")
 
+	def update_canvas(self):
+		"""
+		Given inputted date string of format YYYY-MM-DD, creates a date object from it.
+		Then, use it to slice a copy of loaded stock_data to be used to update graphics.
+		Checks checkboxes first to see if SMA1 and SMA2 lines need to be drawn.
+		Error handling:
+		- Invalid date format: prompts user
+		- Non-existent stock_data: prompts user
+		"""
+		self.date_format = '%Y-%m-%d'
+
+		try:
+			start_date = str(datetime.strptime(self.startDateEdit.text(), self.date_format).date())
+			end_date = str(datetime.strptime(self.endDateEdit.text(), self.date_format).date())
+			period = f"{start_date} to {end_date}"
+			self.periodEdit.setText(period)
+
+			# builds a list of graphs to plot by checking the tickboxes
+			column_headers = ['Close']
+			style = ['k-']
+
+			if self.SMA1Checkbox.isChecked():
+				self.stock_data._calculate_SMA(int(self.SMA1Edit.text()))
+				column_headers.append(f"SMA{self.SMA1Edit.text()}")
+				style.append('b-')
+			if self.SMA2Checkbox.isChecked():
+				self.stock_data._calculate_SMA(int(self.SMA2Edit.text()))
+				column_headers.append(f"SMA{self.SMA2Edit.text()}")
+				style.append('c-')
+			if len(column_headers) == 3:
+				self.stock_data._calculate_crossover(column_headers[1], column_headers[2], column_headers[1])
+				column_headers.append('Sell')
+				style.append('rv')
+				column_headers.append('Buy')
+				style.append('g^')
+
+			self.selected_stock_data = self.stock_data.get_data(start_date, end_date)
+			self.plot_graph(column_headers)
+			self.plot_signals()
+
+			self.report(f"Plotting {column_headers} data from period: {start_date} to {end_date}.")
+			print(self.selected_stock_data)
+
+		except ValueError as e:
+			self.report(f"Time period has not been specified or does not match YYYY-MM-DD format, {e}.")
+
+		except AssertionError as e:
+			self.report(f"Selected range is empty, {e}")
+
+		except KeyError as e:
+			self.report(f"Data for this date does not exist: {e}")
+
+		except Exception as e: self.report(e)
+
+	def _plot_graph(self, column_headers):
+		self.figure.clear()
+		assert not self.selected_stock_data.empty
+
+		# matplotlib has its own internal representation of datetime
+		# date2num converts datetime.datetime to this internal representation
+		x_data = list(mdates.date2num(
+		                              [datetime.strptime(dates, self.date_format).date()
+		                              for dates in self.selected_stock_data.index.values]
+		                              ))
+
+		idx = 0
+		colors = ['black', 'blue', 'orange']
+		ax = self.figure.add_subplot(111)
+		for column_head in column_headers:
+			if column_head in self.selected_stock_data.columns:
+				y_data = list(self.selected_stock_data[column_head])
+				ax.plot(x_data, y_data, label=column_head, color=colors[idx])
+				self.report(f"{column_head} data is being plotted.")
+			else: self.report(f"{column_head} data does not exist.")
+			idx += 1
+
+		# formatting
+		months_locator = mdates.MonthLocator()
+		months_format = mdates.DateFormatter('%b %Y')
+		ax.xaxis.set_major_locator(months_locator)
+		ax.xaxis.set_major_formatter(months_format)
+		ax.format_xdata = mdates.DateFormatter(self.date_format)
+		ax.format_ydata = lambda y: '$%1.2f' % y
+		ax.grid(True)
+		self.figure.autofmt_xdate()
+		self.figure.legend()
+		self.figure.tight_layout()
+		self.canvas.draw()
 
 	def update_graphics(self):
 		"""
@@ -145,7 +233,7 @@ class Main(qtw.QWidget, Ui_Form):
 		for column_head in column_headers:
 			if column_head in self.selected_stock_data.columns:
 				y_data = list(self.selected_stock_data[column_head])
-				ax.plot(x_data, y_data, label=column_head, color=colors[idx])
+				ax.plot(x_data, y_data, label=column_head) #, color=colors[idx])
 				self.report(f"{column_head} data is being plotted.")
 			else: self.report(f"{column_head} data does not exist.")
 			idx += 1
@@ -188,10 +276,6 @@ class Main(qtw.QWidget, Ui_Form):
 		"""
 		report_text = qtw.QLabel(string)
 		self.scrollLayout.addWidget(report_text)
-
-		# scrolls to the latest report
-		latest_index = self.scrollLayout.count()-1
-		self.scrollArea.ensureWidgetVisible(self.scrollLayout.itemAt(latest_index).widget())
 		print(string)
 
 	def center(self):

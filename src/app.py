982a6f9f66ed2b97b14ba16692f00bfd84161dce
heavensward.argentlux <heavensward.argentlux@gmail.com>
2020-11-06 18:39:56 +0800

    fixed plot_graph

--- src/app.py
+++ src/app.py
@@ -20,7 +20,7 @@ class Main(qtw.QWidget, Ui_Form):
 		self.setWindowTitle("Stock Chart & Moving Average Application")
 
 		# sets up a new figure to plot on, then instantiates a canvas and toolbar object
-		self.figure = plt.figure()
+		self.figure, self.ax = plt.subplots()
 		self.canvas = FigureCanvas(self.figure)
 		self.toolbar = NavigationToolbar(self.canvas, self)
 
@@ -63,8 +63,8 @@ class Main(qtw.QWidget, Ui_Form):
 			self.periodEdit.setText(period)
 			self.SMA1Edit.setText("15")
 			self.SMA2Edit.setText("50")
-			self.SMA1Checkbox.setChecked(True)
-			self.SMA2Checkbox.setChecked(True)
+			self.SMA1Checkbox.setChecked(False)
+			self.SMA2Checkbox.setChecked(False)
 
 			self.report(f"Data loaded from {filepath}; period auto-selected: {start_date} to {end_date}.")
 			print(self.stock_data.data)
@@ -94,26 +94,25 @@ class Main(qtw.QWidget, Ui_Form):
 
 			# builds a list of graphs to plot by checking the tickboxes
 			column_headers = ['Close']
-			style = ['k-']
+			formats = ['k-']
 
 			if self.SMA1Checkbox.isChecked():
 				self.stock_data._calculate_SMA(int(self.SMA1Edit.text()))
 				column_headers.append(f"SMA{self.SMA1Edit.text()}")
-				style.append('b-')
+				formats.append('b-')
 			if self.SMA2Checkbox.isChecked():
 				self.stock_data._calculate_SMA(int(self.SMA2Edit.text()))
 				column_headers.append(f"SMA{self.SMA2Edit.text()}")
-				style.append('c-')
+				formats.append('c-')
 			if len(column_headers) == 3:
 				self.stock_data._calculate_crossover(column_headers[1], column_headers[2], column_headers[1])
 				column_headers.append('Sell')
-				style.append('rv')
+				formats.append('rv')
 				column_headers.append('Buy')
-				style.append('g^')
+				formats.append('g^')
 
 			self.selected_stock_data = self.stock_data.get_data(start_date, end_date)
-			self.plot_graph(column_headers)
-			self.plot_signals()
+			self._plot_graph(column_headers, formats)
 
 			self.report(f"Plotting {column_headers} data from period: {start_date} to {end_date}.")
 			print(self.selected_stock_data)
@@ -129,8 +128,8 @@ class Main(qtw.QWidget, Ui_Form):
 
 		except Exception as e: self.report(e)
 
-	def _plot_graph(self, column_headers):
-		self.figure.clear()
+	def _plot_graph(self, column_headers, formats):
+		self.ax.clear()
 		assert not self.selected_stock_data.empty
 
 		# matplotlib has its own internal representation of datetime
@@ -140,135 +139,27 @@ class Main(qtw.QWidget, Ui_Form):
 		                              for dates in self.selected_stock_data.index.values]
 		                              ))
 
-		idx = 0
-		colors = ['black', 'blue', 'orange']
-		ax = self.figure.add_subplot(111)
-		for column_head in column_headers:
-			if column_head in self.selected_stock_data.columns:
-				y_data = list(self.selected_stock_data[column_head])
-				ax.plot(x_data, y_data, label=column_head, color=colors[idx])
-				self.report(f"{column_head} data is being plotted.")
-			else: self.report(f"{column_head} data does not exist.")
-			idx += 1
+		colors = ['black', 'blue', 'orange', 'red', 'green']
+		# ax = self.figure.add_subplot(111)
+		for i in range(len(column_headers)):
+			if column_headers[i] in self.selected_stock_data.columns:
+				y_data = list(self.selected_stock_data[column_headers[i]])
+				self.ax.plot(x_data, y_data, formats[i], label=column_headers[i], color=colors[i]) # , color=colors[idx])
+				self.report(f"{column_headers[i]} data is being plotted.")
+			else: self.report(f"{column_headers[i]} data does not exist.")
 
 		# formatting
 		months_locator = mdates.MonthLocator()
 		months_format = mdates.DateFormatter('%b %Y')
-		ax.xaxis.set_major_locator(months_locator)
-		ax.xaxis.set_major_formatter(months_format)
-		ax.format_xdata = mdates.DateFormatter(self.date_format)
-		ax.format_ydata = lambda y: '$%1.2f' % y
-		ax.grid(True)
+		self.ax.xaxis.set_major_locator(months_locator)
+		self.ax.xaxis.set_major_formatter(months_format)
+		self.ax.format_xdata = mdates.DateFormatter(self.date_format)
+		self.ax.format_ydata = lambda y: '$%1.2f' % y
+		self.ax.grid(True)
 		self.figure.autofmt_xdate()
 		self.figure.legend()
 		self.figure.tight_layout()
 		self.canvas.draw()
-
-	def update_graphics(self):
-		"""
-		Given inputted date string of format YYYY-MM-DD, creates a date object from it.
-		Then, use it to slice a copy of loaded stock_data to be used to update graphics.
-		Checks checkboxes first to see if SMA1 and SMA2 lines need to be drawn.
-		Error handling:
-		- Invalid date format: prompts user
-		- Non-existent stock_data: prompts user
-		"""
-		self.date_format = '%Y-%m-%d'
-
-		try:
-			start_date = datetime.strptime(self.startDateEdit.text(), self.date_format).date()
-			end_date = datetime.strptime(self.endDateEdit.text(), self.date_format).date()
-			period = f"{start_date} to {end_date}"
-			self.periodEdit.setText(period)
-
-			# builds a list of graphs to plot by checking the tickboxes
-			column_headers = ['Close']
-			if self.SMA1Checkbox.isChecked():
-				column_headers.append("SMA" + self.SMA1Edit.text())
-				self.stock_data._calculate_SMA(int(self.SMA1Edit.text()))
-			if self.SMA2Checkbox.isChecked():
-				column_headers.append("SMA" + self.SMA2Edit.text())
-				self.stock_data._calculate_SMA(int(self.SMA2Edit.text()))
-
-			self.report(f"Time period specified as: {period}. Plotting...")
-
-			try:
-				if len(column_headers) == 3:
-					self.stock_data._calculate_crossover(column_headers[1], column_headers[2], column_headers[1])
-
-				self.selected_stock_data = self.stock_data.get_data(start_date, end_date)
-				print(self.selected_stock_data)
-				self.plot_graph(column_headers)
-				self.plot_signals()
-
-			except AssertionError as e:
-				self.report(f"Selected range is empty, {e}.")
-
-			except AttributeError as e:
-				self.report(f"Stock data has not been loaded. Please specify filepath of relevant *.csv file, {e}.")
-
-		except ValueError as e:
-			self.report(f"Time period has not been specified or does not match YYYY-MM-DD format, {e}.")
-
-	def plot_graph(self, column_headers):
-		"""
-		Given non-empty selected_stock_data and a specified column_head name,
-		plots the graph in the canvas
-		Error handling:
-		- Empty y_data: raise AssertionError
-		"""
-		self.figure.clear()
-		assert not self.selected_stock_data.empty
-
-		# matplotlib has its own internal representation of datetime
-		# date2num converts datetime.datetime to this internal representation
-		x_data = list(mdates.date2num(
-		                              [datetime.strptime(dates, self.date_format).date()
-		                              for dates in self.selected_stock_data.index.values]
-		                              ))
-
-		idx = 0
-		colors = ['black', 'blue', 'orange']
-		ax = self.figure.add_subplot(111)
-		for column_head in column_headers:
-			if column_head in self.selected_stock_data.columns:
-				y_data = list(self.selected_stock_data[column_head])
-				ax.plot(x_data, y_data, label=column_head) #, color=colors[idx])
-				self.report(f"{column_head} data is being plotted.")
-			else: self.report(f"{column_head} data does not exist.")
-			idx += 1
-
-		# formatting
-		months_locator = mdates.MonthLocator()
-		months_format = mdates.DateFormatter('%b %Y')
-		ax.xaxis.set_major_locator(months_locator)
-		ax.xaxis.set_major_formatter(months_format)
-		ax.format_xdata = mdates.DateFormatter(self.date_format)
-		ax.format_ydata = lambda y: '$%1.2f' % y
-		ax.grid(True)
-		self.figure.autofmt_xdate()
-		self.figure.legend()
-		self.figure.tight_layout()
-		self.canvas.draw()
-
-	def plot_signals(self):
-		# matplotlib has its own internal representation of datetime
-		# date2num converts datetime.datetime to this internal representation
-		isChecked = self.SMA1Checkbox.isChecked() and self.SMA2Checkbox.isChecked()
-		if ('Buy' and 'Sell' in self.selected_stock_data.columns) and isChecked:
-			x_data = list(mdates.date2num(
-			                              [datetime.strptime(dates, self.date_format).date()
-			                              for dates in self.selected_stock_data.index.values]
-			                              ))
-			ybuy_data = list(self.selected_stock_data['Buy'])
-			ysell_data = list(self.selected_stock_data['Sell'])
-			ax = self.figure.add_subplot(111)
-			ax.plot(x_data, ybuy_data, label='Buy', color='green', markersize=10, marker='^')
-			ax.plot(x_data, ysell_data, label='Sell', color='red', markersize=10, marker='v')
-
-			self.figure.legend()
-			self.figure.tight_layout()
-			self.canvas.draw()
 
 	def report(self, string):
 		"""
